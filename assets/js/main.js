e67d97346ced75387daec5fedd9eb176835bd0f8
Thaddeus Toledo <toledothaddeusf@gmail.com>
2025-05-04 13:13:53 +0800

    Added a draft sidebar for template.html

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+const menu_toggle = document.getElementById('menu-toggle');
+const sidebar = document.getElementById('sidebar');
+
+menu_toggle.addEventListener('click', () => {
+    sidebar.classList.toggle('-translate-x-full');
+});
+
+document.addEventListener('click', (event) => {
+    if (!menu_toggle.contains(event.target) && !sidebar.contains(event.target)) {
+        sidebar.classList.add('-translate-x-full');
+    }
+});
