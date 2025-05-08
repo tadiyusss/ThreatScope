aad0093d2659ee021bbc388cf8f31159642dd16c
Thaddeus Toledo <toledothaddeusf@gmail.com>
2025-05-08 12:15:25 +0800

    design: Moved the sidebar to the right and it is hidden by default

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,13 @@
 const menu_toggle = document.getElementById('menu-toggle');
 const sidebar = document.getElementById('sidebar');
 
-sidebar.classList.remove('-translate-x-full'); 
 
 menu_toggle.addEventListener('click', () => {
-    sidebar.classList.toggle('-translate-x-full');
+    sidebar.classList.toggle('translate-x-full');
 });
 
 document.addEventListener('click', (event) => {
     if (!menu_toggle.contains(event.target) && !sidebar.contains(event.target)) {
-        sidebar.classList.add('-translate-x-full');
+        sidebar.classList.add('translate-x-full');
     }
 });
